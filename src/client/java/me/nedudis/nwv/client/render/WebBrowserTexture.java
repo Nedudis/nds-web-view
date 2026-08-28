1f959c08587756b5d660e45e02ad909975e6ceb8
Nedudis <nedasleskauskas@gmail.com>
2026-08-29 02:27:35 +0300

    Fixed crash that happened on world rejoining.

--- src/client/java/me/nedudis/nwv/client/render/WebBrowserTexture.java
+++ src/client/java/me/nedudis/nwv/client/render/WebBrowserTexture.java
@@ -2,8 +2,12 @@ package me.nedudis.nwv.client.render;
 
 import com.mojang.blaze3d.textures.GpuTextureView;
 import net.dimaskama.mcef.api.MCEFBrowser;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.AbstractTexture;
+import net.minecraft.client.renderer.texture.MissingTextureAtlasSprite;
+import org.jspecify.annotations.NullMarked;
 
+@NullMarked
 public class WebBrowserTexture extends AbstractTexture {
     private final MCEFBrowser browser;
 
@@ -13,6 +17,12 @@ public class WebBrowserTexture extends AbstractTexture {
 
     @Override
     public GpuTextureView getTextureView() {
-        return this.browser.getTextureView();
+        GpuTextureView view = this.browser.getTextureView();
+        if (view != null) return view;
+
+        return Minecraft.getInstance()
+                .getTextureManager()
+                .getTexture(MissingTextureAtlasSprite.getLocation())
+                .getTextureView();
     }
 }
